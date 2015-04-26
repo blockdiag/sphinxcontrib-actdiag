e00280daffabb6a07bda2b10caee23f465603efc
Takeshi KOMIYA <i.tkomiya@gmail.com>
2015-04-26 22:09:10 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = ['blockdiag>=1.5.0', 'actdiag>=0.5.3', 'Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-actdiag',
-    version='0.8.2',
+    version='0.8.3',
     url='http://github.com/tk0miya/sphinxcontrib-actdiag',
     license='BSD',
     author='Takeshi KOMIYA',
